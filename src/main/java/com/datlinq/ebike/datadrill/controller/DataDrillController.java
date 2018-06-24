8a94e81d404709d10c6bbb177f7438d81fc491b7
gnewball <gnewball11@gmail.com>
2018-06-24 16:05:30 +0200

    run queries trough aprache drill REST added

--- src/main/java/com/datlinq/ebike/datadrill/controller/DataDrillController.java
+++ src/main/java/com/datlinq/ebike/datadrill/controller/DataDrillController.java
@@ -1,17 +1,28 @@
 package com.datlinq.ebike.datadrill.controller;
 
+import org.apache.camel.Exchange;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.datlinq.ebike.datadrill.service.DataDrillService;
+
 @RestController
 public class DataDrillController {
 
-	@GetMapping("/datlinq/restaurant/ebikes")
+	@Autowired
+	DataDrillService service;
+	
+	@GetMapping("/restaurant/ebikes")
 	public ResponseEntity getRestaurantsForEBikes(@RequestParam String country,@RequestParam String city) {
 		
-		return ResponseEntity.ok().build();
+		Exchange result = service.getRestaurantsForEBikes(country, city);
+		
+		return ResponseEntity
+		.status(Integer.parseInt(result.getIn().getHeader(Exchange.HTTP_RESPONSE_CODE).toString()))
+		.body(result.getIn().getBody());
 	}
 	
 	
--- src/main/java/com/datlinq/ebike/datadrill/service/DataDrillService.java
+++ src/main/java/com/datlinq/ebike/datadrill/service/DataDrillService.java
@@ -2,33 +2,29 @@ package com.datlinq.ebike.datadrill.service;
 
 import org.apache.camel.Exchange;
 import org.apache.camel.FluentProducerTemplate;
-import org.apache.camel.component.file.GenericFileOnCompletion;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 
 import com.datlinq.ebike.datadrill.model.DataQuery;
 
 @Service
 public class DataDrillService {
-	
+
 	@Autowired
 	private FluentProducerTemplate fluentProducerTemplate;
 
-	
-	public void getRestaurantsForEBikes(String Country, String city){
-		
+	public Exchange getRestaurantsForEBikes(String country, String city) {
+
 		DataQuery dataQuery = new DataQuery();
 		dataQuery.setQuery("SELECT * FROM cp.`employee.json` LIMIT 5");
 		dataQuery.setQueryType("SQL");
-		Exchange result = fluentProducerTemplate
-				.withBody(dataQuery)
-//			    .withHeader("address", address)
-			    .to("direct:datadrill").send();
-		
-//		return ResponseEntity
-//				.status(Integer.parseInt(result.getIn().getHeader(Exchange.HTTP_RESPONSE_CODE).toString()))
-//				.body(result.getIn().getBody());
+		Exchange result = fluentProducerTemplate.withBody(dataQuery)
+				 .withHeader(Exchange.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+				 .withHeader("User-Name", "ebike-data-drill")
+				.to("direct:datadrill").send();
+		return result;
+
 	}
 
 }
